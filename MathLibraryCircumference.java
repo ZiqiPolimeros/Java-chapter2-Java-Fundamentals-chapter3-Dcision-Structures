1a1fb76764d735b79b0d7622aee29c80824eac01
Ziqi Wu <55024117+ZiqiPolimeros@users.noreply.github.com>
2019-10-05 13:21:44 -0400

    Add files via upload

--- MathLibraryCircumference.java
+++ MathLibraryCircumference.java
@@ -15,17 +15,30 @@ public class MathLibraryCircumference {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        double diameter;
+        double circumference;
+        String input;
+        char repeat;
     
+        do{
         System.out.println("Welcome to Math Library --- Circumference Calculator!\n" +
                            "Please type in the diameter of a circle.\n "+
-                           "Remember to press ENTER for your answer. \n");
-        double diameter;
+                           "Remember to press ENTER for your answer. \n"); 
         Scanner d = new Scanner(System.in);
         diameter = d.nextDouble();
-        double circumference = diameter * Math.PI;
+        circumference = diameter * Math.PI;
         DecimalFormat twoDForm = new DecimalFormat("#.00");
         System.out.println("The circumferece is "+ twoDForm.format(circumference) +".");
-        
-    }
+        System.out.println("");
+        System.out.println("Would you like to calculate " + 
+                    "another circle?"); 
+            System.out.print("Enter Y for yes or N for no: "); 
+            input = d.nextLine();
+            input = d.nextLine();            
+            repeat = input.charAt(0);       
+    } while (repeat == 'Y' || repeat == 'y'); 
     
+                }
 }
+
